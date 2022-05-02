f058483fc15fc53b2ff64e1d579a191b6a91b81f
Cletus Emmanuel <68257939+Ci-re@users.noreply.github.com>
2022-05-02 11:07:49 +0000

    Add files via upload
    adsfads

---
+++ starter/controllers/main.js
@@ -0,0 +1,36 @@
+// check username, password in post(login) request
+// if exist create new JWT
+// send back to front-end
+// setup authentication so only the request with JWT can access the 
+// dashboard
+const jwt = require("jsonwebtoken")
+const {BadRequestError} = require('../errors')
+const login = async (req, res) => {
+    const {username, password} = req.body
+     // mongoose validation 
+    // Joi
+    // check in the controller
+    if(!username || !password) {
+        throw new BadRequestError('Please provide email and password')
+    }
+    const id = new Date().getDate();
+    const token  = jwt.sign(
+        {id, username}, process.env.JWT_SECRET, {expiresIn: '30d'}
+    )
+    console.log(token)
+    console.log(username, password)
+    res.status(200).json({msg: "User created", token})
+}
+
+const dashboard = async (req, res) => {
+    console.log(req.user)
+    const luckyNumber = Math.floor(Math.random() * 100) 
+    res.status(200).json({
+        msg: `You're welcome ${req.user.username}`,
+        secret: `Your lucky number is ${luckyNumber}`
+    })
+}
+
+module.exports = {
+    login, dashboard
+}
